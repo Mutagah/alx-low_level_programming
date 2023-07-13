be44eb3ef30ab63012a31416bb287c762711656e
Mutagah <kihahumutagah@gmail.com>
2023-07-13 11:42:44 -0700

    The 101-quote.c file edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of artis useful\" - Dora Korpar, 2015-10-19"
+ * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
