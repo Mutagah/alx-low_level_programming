5ec5b5ce6526b32f326b5117f5750cad09454762
Mutagah <kihahumutagah@gmail.com>
2023-07-13 11:33:56 -0700

    THe quote task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints "and that piece of artis useful\" - Dora Korpar, 2015-10-19"
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19", 58)
+	return (1);
+}
