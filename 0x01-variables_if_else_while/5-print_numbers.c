66510d5c1610cb18f259cb2a574aeb04f80ed11d
Mutagah <kihahumutagah@gmail.com>
2023-07-14 13:51:07 -0700

    Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ * Return: always a zero (success)
+ */
+int main(void)
+{
+int number;
+	for (number = 0; number <= 9; number++)
+		printf("%d", number);
+	printf("\n");
+}
