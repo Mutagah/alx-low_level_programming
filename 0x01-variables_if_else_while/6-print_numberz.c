6e8c63f31d8ef3216fae83f5027f253c4c20851b
Mutagah <kihahumutagah@gmail.com>
2023-07-14 14:25:33 -0700

    Prints single digit numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers from 0 to 9
+ * Return: always a zero (success)
+ */
+int main(void)
+{
+int number;
+	for (number = 48; number <= 57; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
