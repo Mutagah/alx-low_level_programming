ce67418dfdcd300be7c5383dc0ea6db6a9b9d574
Mutagah <kihahumutagah@gmail.com>
2023-07-19 10:57:56 -0700

    Created the 2-main.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+int times = 1;
+	while (times <= 10)
+	{
+		alphabet = 'a';
+		while (alphabet < 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+	putchar('\n');
+	times++;
+	}
+}
