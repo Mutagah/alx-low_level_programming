61ca0d330c90aeea3e2cd66abe72b95ee473df95
Mutagah <kihahumutagah@gmail.com>
2023-07-19 12:56:22 -0700

    The sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the paratemer subjected to a check
+ * Return: 1 when n greater than zero
+ * 0 when n is zero
+ * -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
