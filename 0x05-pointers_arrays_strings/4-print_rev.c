59cf4e7dd332a0a1cafccaaf7b5418fa87a39d19
Mutagah <kihahumutagah@gmail.com>
2023-07-26 13:24:10 -0700

    Print reverse function edit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int word_count = strlen(s);
+	int word_count = strlen(s) - 1;
 
-	while (word_count - 1 >= 0)
+	while (word_count >= 0)
 	{
 		_putchar(s[word_count]);
 		word_count--;
