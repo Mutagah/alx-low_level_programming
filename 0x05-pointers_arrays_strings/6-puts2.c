d7b45a14162b4a850a8da386d37006bfca89f0e4
Mutagah <kihahumutagah@gmail.com>
2023-07-26 16:36:03 -0700

    Printing characters in a string that fall under an even index number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
- *
+ * puts2 - prints the second character in a string
+ * @str: The input string that is to be splitted
  */
 void puts2(char *str)
 {
 	int counter;
-	int string_length = strlen(str);
+	int string_length = strlen(str) - 1;
+
 	for (counter = 0; counter <= string_length; counter++)
 	{
 		if (counter % 2 == 0)
