298dde6dbf8f7779fc487c356d016938d913f6f0
Mutagah <kihahumutagah@gmail.com>
2023-07-26 13:15:28 -0700

    Printing in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
+ * print_rev - prints a string in reverse
+ * @s: The input character that is has an argurment as a string.
  */
 void print_rev(char *s)
 {
-	int word_count = strlen(s);	
-	while ( word_count >= 0)
+	int word_count = strlen(s);
+
+	while (word_count >= 0)
 	{
 		_putchar(s[word_count]);
 		word_count--;
 	}
 	_putchar('\n');
 }
-
-	
