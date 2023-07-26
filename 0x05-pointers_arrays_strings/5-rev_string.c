84c76a61c5fce27c2c5b0cb4dd423788a2d43678
Mutagah <kihahumutagah@gmail.com>
2023-07-26 15:38:46 -0700

    The reversal of string task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Reversal of a a given string
+ * @s: The input string that is expected to be reversed
+*/
+void rev_string(char *s)
+{
+	int end = strlen(s) - 1;
+	int start = 0;
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
