035c7516219049ea1d92bc87877035d374b1c1ed
Mutagah <kihahumutagah@gmail.com>
2023-07-26 17:40:19 -0700

    The last half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints the second half of a string
+ * @str: The string that would be splitted and printed the last half bit
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+
+	int plength = length % 2 == 0 ? length / 2 : (length - 1) / 2;
+
+	for (; plength <= length; plength++)
+		_putchar(str[plength]);
+	_putchar('\n');
+}
