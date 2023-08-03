76dfc719d656470ffe0c2a24dd70bd9f5d44e2ba
Mutagah <kihahumutagah@gmail.com>
2023-08-03 06:54:47 -0700

    Betty error correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,9 +24,7 @@ int _sqrt_generation(int n, int root)
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return (-1);
-        else
-                return (_sqrt_generation(n, 0));
+	if (n < 0)
+		return (-1);
+	return (_sqrt_generation(n, 0));
 }
-
