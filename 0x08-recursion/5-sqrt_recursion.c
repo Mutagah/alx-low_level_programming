13459a1cdc8329fcc0140aadc9406f56a095ad80
Mutagah <kihahumutagah@gmail.com>
2023-08-03 06:50:38 -0700

    Calculation of the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_generation - Recursively looks for a the square root of n
+ * @n: The number to check for its natural sqrt
+ * @root: The root that increments with one gradually
+ * Return: Integer, which is the root or -1
+ */
+int _sqrt_generation(int n, int root)
+{
+	if (root * root > n)
+	{
+		return (-1);
+	}
+	if (root * root == n)
+	{
+		return (root);
+	}
+	return (_sqrt_generation(n, root + 1));
+}
+/**
+ * _sqrt_recursion - checks if n is less than 0 else the sqrt is generated
+ * @n: The number to check its natural sqrt
+ * Return: Invokes the sqrt_generation function
+ */
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+                return (-1);
+        else
+                return (_sqrt_generation(n, 0));
+}
+
